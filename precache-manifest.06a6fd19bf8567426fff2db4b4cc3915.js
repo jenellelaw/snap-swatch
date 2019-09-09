796f425ee648b114ecf6e1e7a7e1d839f8d4ea38
Jenelle <lawjenelle@gmail.com>
2019-09-09 17:24:10 -0400

    Updates

---
+++ precache-manifest.06a6fd19bf8567426fff2db4b4cc3915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4467920590bf5d32b1b5a241d28d5d0e",
+    "url": "/jenelle-law-project-5/index.html"
+  },
+  {
+    "revision": "243be09b4dda3619027b",
+    "url": "/jenelle-law-project-5/static/css/main.5df3ab31.chunk.css"
+  },
+  {
+    "revision": "a4664484468dbc4a3d27",
+    "url": "/jenelle-law-project-5/static/js/2.f4ae13f1.chunk.js"
+  },
+  {
+    "revision": "243be09b4dda3619027b",
+    "url": "/jenelle-law-project-5/static/js/main.224fc5b1.chunk.js"
+  },
+  {
+    "revision": "0268e23bd1a40884b3e6",
+    "url": "/jenelle-law-project-5/static/js/runtime~main.15c35931.js"
+  }
+]);
